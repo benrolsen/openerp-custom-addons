6ef6d5052012ed22648abe90960cfdb716174b20
Ben Olsen <ben.olsen@imsar.com>
2014-12-11 11:33:28 -0700

    tweak the subrouting views a bit

--- imsar_timekeeping/report/reports.py
+++ imsar_timekeeping/report/reports.py
@@ -5,42 +5,14 @@ import openerp.addons.decimal_precision as dp
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
 
 
-class payroll_report_wizard(models.TransientModel):
-    _name = "hr.timekeeping.payroll.report.wizard"
-    _description = "Timekeeping Entries Statistics"
-
-    year = fields.Integer('Year', default=lambda self: datetime.now().year)
-    payperiod_id = fields.Many2one('hr.timekeeping.payperiod', 'Pay Period', required=True, default=lambda self: self.default_payperiod())
-
-    @api.model
-    def default_payperiod(self):
-        today = datetime.today()
-        this_payperiod = self.env['hr.timekeeping.payperiod'].get_payperiod(today)
-        prev_day = datetime.strptime(this_payperiod.start_date, DATE_FORMAT) + timedelta(days=-1)
-        last_payperiod = self.env['hr.timekeeping.payperiod'].get_payperiod(prev_day)
-        return last_payperiod or ''
-
-    @api.multi
-    def open_report(self):
-        ctx = {}
-        if self._context:
-            ctx = self._context.copy()
-        return {
-            'name': _('Payroll Report'),
-            'type': 'ir.actions.act_window',
-            'res_model': 'hr.timekeeping.payroll.report',
-            'view_type': 'form',
-            'view_mode': 'graph,tree',
-            'domain': "[('date', '>=', '{}'),('date','<=','{}')]".format(self.payperiod_id.start_date, self.payperiod_id.end_date),
-            'context': ctx,
-        }
-
-
-class timekeeping_payroll_report(models.Model):
-    _name = "hr.timekeeping.payroll.report"
+class timekeeping_lines_report(models.Model):
+    _name = "hr.timekeeping.lines.report"
     _description = "Timekeeping Entries Statistics"
     _auto = False
 
+    week_name = fields.Char('Week Number')
+    # type = fields.Selection([('regular','Regular'),('addendum','Addendum'),('proxy','Proxy'),], 'Type', default='regular', required=True, readonly=True,)
+    payperiod_id = fields.Many2one('hr.timekeeping.payperiod', 'Pay Period')
     date = fields.Date('Date', readonly=True)
     user_id = fields.Many2one('res.users', string='User', readonly=True)
     unit_amount = fields.Float('Quantity', readonly=True)
@@ -48,14 +20,38 @@ class timekeeping_payroll_report(models.Model):
     worktype = fields.Many2one('hr.timekeeping.worktype', string="Work Type", readonly=True)
     state = fields.Char('Timesheet State', readonly=True)
     task_code = fields.Char('Task Code', readonly=True)
+    adv_search = fields.Char('Advanced Filter Search', compute='_computed_fields', search='_adv_search')
+
+    @api.one
+    def _computed_fields(self):
+        self.adv_search = ''
+
+    def _adv_search(self, operator, value):
+        today = date.today()
+        this_payperiod = self.env['hr.timekeeping.payperiod'].get_payperiod(today)
+        prev_payperiod_date = datetime.strptime(this_payperiod.start_date, DATE_FORMAT) + timedelta(days=-1)
+        prev_payperiod = self.env['hr.timekeeping.payperiod'].get_payperiod(prev_payperiod_date)
+        week_ab = this_payperiod.get_week_ab(today)
+
+        ids = []
+        if value == 'this_week':
+            this_week = this_payperiod.name + '-' + week_ab
+            ids = self.search([('week_name','=',this_week)]).ids
+        elif value == 'this_payperiod':
+            ids = self.search([('payperiod_id','=',this_payperiod.id)]).ids
+        elif value == 'prev_payperiod':
+            ids = self.search([('payperiod_id','=',prev_payperiod.id)]).ids
+        else:
+            ids = self.search([]).ids
+        return [('id','in',ids)]
 
     @api.cr
     def init(self, cr):
-        tools.drop_view_if_exists(cr, 'hr_timekeeping_payroll_report')
+        tools.drop_view_if_exists(cr, 'hr_timekeeping_lines_report')
         # left join timekeeping_line_move_line_rel to see which have move lines?
         # join move lines and check for reconciled/unreconciled?
         cr.execute("""
-            create or replace view hr_timekeeping_payroll_report as (
+            create or replace view hr_timekeeping_lines_report as (
                 select
                     min(tl.id) as id,
                     tl.date as date,
@@ -64,6 +60,8 @@ class timekeeping_payroll_report(models.Model):
                     sum(tl.amount + tl.premium_amount) as amount,
                     tl.worktype as worktype,
                     ts.state as state,
+                    ts.name as week_name,
+                    ts.payperiod_id as payperiod_id,
                     (ar.name || ' - ' || aa.name) as task_code
                 from
                     hr_timekeeping_line tl
@@ -72,7 +70,7 @@ class timekeeping_payroll_report(models.Model):
                 join account_routing_subrouting ars on tl.routing_subrouting_id = ars.id
                 join account_analytic_account aa on ars.account_analytic_id = aa.id
                 group by
-                    tl.date, tl.user_id, tl.worktype, ts.state, task_code
+                    tl.date, tl.user_id, tl.worktype, ts.state, ts.payperiod_id, week_name, task_code
             )
         """)
 
